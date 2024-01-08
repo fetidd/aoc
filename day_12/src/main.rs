3be8a80b8ecc5c9679eac9b300836de1ee08bebb
Ben Jones <ben@reticent-monolith.com>
2024-01-08 19:16:25 +0000

    test inout in main file

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -1,7 +1,12 @@
 use day_12::*;
 
 static INPUT: &str = include_str!("../input.txt");
-static TEST_INPUT: &str = include_str!("../test_input.txt");
+static TEST_INPUT: &str = "???.### 1,1,3\n\
+                           .??..??...?##. 1,1,3\n\
+                           ?#?#?#?#?#?#?#? 1,3,1,6\n\
+                           ????.#...#... 4,1,1\n\
+                           ????.######..#####. 1,6,5\n\
+                           ?###???????? 3,2,1";
 
 fn main() {
     get_lines(TEST_INPUT)
