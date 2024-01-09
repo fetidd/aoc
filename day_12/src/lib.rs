5bfdb9015e90527a0b41d9e9d89eb035a38a4287
Ben Jones <fetiddius@gmail.com>
2024-01-09 08:48:31 +0000

    jdjdjd

--- day_12/src/lib.rs
+++ day_12/src/lib.rs
@@ -1,9 +1,9 @@
-mod item;
-use item::Item;
+mod spring;
 use itertools::Itertools;
+use spring::SpringGroup;
 
 /// Function to get the day 12 lines in a tuple of items and groups from a string
-pub fn get_lines(source: &str) -> Vec<(Vec<Item>, Vec<i32>)> {
+pub fn get_lines(source: &str) -> Vec<(Vec<SpringGroup>, Vec<i32>)> {
     source
         .lines()
         .map(|line| {
@@ -11,13 +11,35 @@ pub fn get_lines(source: &str) -> Vec<(Vec<Item>, Vec<i32>)> {
                 .split(" ")
                 .collect_tuple::<(&str, &str)>()
                 .expect("failed to create line tuple"); // panic if input badly formed
-            (
-                springs.chars().map(Item::from).collect::<Vec<Item>>(),
-                groups
-                    .split(",")
-                    .map(|n| n.parse::<i32>().expect("failed parsing group"))
-                    .collect::<Vec<_>>(),
-            )
+            (parse_springs(springs), parse_groups(groups))
         })
         .collect()
+}
+
+fn parse_groups(groups: &str) -> Vec<i32> {
+    groups
+        .split(",")
+        .map(|n| n.parse::<i32>().expect("failed parsing group"))
+        .collect::<Vec<_>>()
+}
+
+fn parse_springs(springs: &str) -> Vec<SpringGroup> {
+    let mut i = 0usize;
+    let mut groups = vec![];
+    let springs = springs.chars().collect::<Vec<_>>();
+    while i < springs.len() {
+        // input will always be ascii so this is safe
+        let g: SpringGroup = springs[i..].into();
+        i += g.len();
+        groups.push(g);
+    }
+    groups
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_springs() {}
 }
---
+++ day_12/src/spring.rs
@@ -0,0 +1,65 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Spring {
+    Working,
+    Unknown,
+    Damaged,
+}
+
+impl From<char> for Spring {
+    fn from(value: char) -> Self {
+        match value {
+            '.' => Spring::Working,
+            '?' => Spring::Unknown,
+            '#' => Spring::Damaged,
+            _ => panic!("received bad spring character"),
+        }
+    }
+}
+impl From<&char> for Spring {
+    fn from(value: &char) -> Self {
+        match *value {
+            '.' => Spring::Working,
+            '?' => Spring::Unknown,
+            '#' => Spring::Damaged,
+            _ => panic!("received bad spring character"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_item_from_char() {
+        assert_eq!(Spring::Working, Spring::from('.'));
+        assert_eq!(Spring::Unknown, Spring::from('?'));
+        assert_eq!(Spring::Damaged, Spring::from('#'));
+    }
+}
+
+pub struct SpringGroup(Vec<Spring>);
+
+impl SpringGroup {
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+impl From<&[char]> for SpringGroup {
+    fn from(value: &[char]) -> Self {
+        let mut chars = value.iter().peekable();
+        let first = chars.peek().expect("no chars to parse!");
+        let springs = chars
+            .take_while(|ch| {
+                if **first == '.' {
+                    **ch == '.'
+                } else {
+                    **ch == '#' || **ch == '?'
+                }
+            })
+            .map(Spring::from)
+            .collect::<Vec<_>>();
+        SpringGroup(springs)
+    }
+}
