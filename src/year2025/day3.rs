4110d92f9554f15eba201585ecdbafc3e08a6841
Ben Jones <fetidd@gmail.com>
2025-12-04 18:59:17 +0000

    still refactoring

--- src/year2025/day3.rs
+++ src/year2025/day3.rs
@@ -1,3 +1,5 @@
+use std::ops::{Index, IndexMut};
+
 pub fn run(input: &str, part: u8) -> String {
     let batt_len = if part == 1 { 2 } else { 12 };
     input.lines().map(|l| find_joltage(l, batt_len)).sum::<u64>().to_string()
@@ -6,50 +8,86 @@ pub fn run(input: &str, part: u8) -> String {
 const MAX_JOLTAGE: u8 = 57;
 const ZERO: u8 = 48;
 
-fn find_joltage(line: &str, battery_num: usize) -> u64 {
-    let mut b = vec![0; battery_num];
-    let mut li = 0;
-    let mut max_ri = 0;
-    let line = line.as_bytes();
-    'outer: while li < line.len() - 1 {
-        let l = line[li];
-        if l > b[0] {
-            // new stronger first battery
-            b[0] = l;
-            if li >= max_ri {
-                // found a stronger first battery after the strongest second, so need a new right
-                b[1] = 0;
-                max_ri = 0;
-            }
+struct Batteries(Vec<(u8, usize)>);
+impl Batteries {
+    fn new(battery_num: usize) -> Self {
+        Self(vec![(0, 0); battery_num])
+    }
+
+    fn zero(&mut self, index: usize) {
+        for i in index..self.0.len() {
+            self.0[i] = (0, 0);
         }
-        let mut ri = li + 1;
-        while ri < line.len() {
-            let r = line[ri];
-            if ri < line.len() - 1 && r > b[0] {
-                b[0] = r;
-                li = ri;
-                b[1] = 0;
-                max_ri = 0;
-                continue 'outer;
+    }
+
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    fn iter(&self) -> impl Iterator<Item = &(u8, usize)> {
+        self.0.iter()
+    }
+
+    fn into_iter(self) -> impl Iterator<Item = (u8, usize)> {
+        self.0.into_iter()
+    }
+}
+
+impl Index<usize> for Batteries {
+    type Output = (u8, usize);
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+
+impl IndexMut<usize> for Batteries {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
+    }
+}
+
+fn find_joltage(line: &str, battery_num: usize) -> u64 {
+    let mut batteries = Batteries::new(battery_num);
+    let mut first_battery_index = 0;
+    let mut furthest_cursor_index = 0;
+    let line = line.as_bytes();
+
+    'line_loop: while first_battery_index < line.len() - 1 {
+        let first_battery = line[first_battery_index];
+        if first_battery > batteries[0].0 {
+            batteries[0].0 = first_battery;
+            // if first_battery_index >= furthest_cursor_index {
+            //     batteries.zero(1);
+            //     furthest_cursor_index = first_battery_index;
+            // }
+        }
+        let mut cursor_index = first_battery_index + 1;
+        while cursor_index < line.len() {
+            let cursor_battery = line[cursor_index];
+            if cursor_index < line.len() - (batteries.len() - 1) && cursor_battery > batteries[0].0 {// found a stronger battery to use as the first
+                batteries[0].0 = cursor_battery;
+                first_battery_index = cursor_index;
+                batteries.zero(1);
+                furthest_cursor_index = cursor_index;
+                continue 'line_loop;
             }
-            if r > b[1]{
+            if cursor_battery > batteries[1].0{
                 // new stronger second battery
-                b[1] = r;
-                max_ri = ri;
+                batteries[1].0 = cursor_battery;
+                furthest_cursor_index = cursor_index;
             }
-            // println!("{max_l} at {li}, {max_r} at {ri} - {line:?}");
-            if b[1] == MAX_JOLTAGE {
-                // strongest second battery we'll get
+            if batteries[1].0 == MAX_JOLTAGE {
                 break;
             }
-            ri += 1;
+            cursor_index += 1;
         }
-        if b[0] == MAX_JOLTAGE && b[1] == MAX_JOLTAGE {
-            break; // got a 99, so cannot be a better battery in this line
+        if batteries.iter().all(|(b, _)| *b == MAX_JOLTAGE) {
+            break 'line_loop;
         }
-        li += 1;
+        first_battery_index += 1;
     }
-    b.into_iter().map(|b| (b - ZERO).to_string()).collect::<String>().parse().unwrap()
+    batteries.into_iter().map(|(b, _)| (b - ZERO).to_string()).collect::<String>().parse().unwrap()
 }
 
 #[cfg(test)]
@@ -63,6 +101,11 @@ mod test {
         assert_eq!(78, find_joltage("234234234234278", 2));
         assert_eq!(92, find_joltage("818181911112111", 2));
         assert_eq!(92, find_joltage("121281212912121", 2));
+        // assert_eq!(987, find_joltage("987654321111111", 3));
+        // assert_eq!(819, find_joltage("811111111111119", 3));
+        // assert_eq!(478, find_joltage("234234234234278", 3));
+        // assert_eq!(921, find_joltage("818181911112111", 3));
+        // assert_eq!(922, find_joltage("121281212912121", 3));
         // assert_eq!(987654321111, find_joltage("987654321111111", 12));
         // assert_eq!(811111111119, find_joltage("811111111111119", 12));
         // assert_eq!(434234234278, find_joltage("234234234234278", 12));
